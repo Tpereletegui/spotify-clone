9cb4003ad69ea7360b083370e6365c508f59a8b7
Tomas Pereletegui <101584073+tomaspereletegui@users.noreply.github.com>
2022-04-10 16:54:21 -0300

    check

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 
 export async function middleware(req){
-  const token = await getToken({req, secret: process.env.JWT_SECRET});
+  const token = await getToken({req, secureCookie: process.env.NEXTAUTH_URL?.startsWith("https://") ?? !!process.env.VERCEL_URL});
   const{pathname}=req.nextUrl
 
   // allow the request if the following it's true
